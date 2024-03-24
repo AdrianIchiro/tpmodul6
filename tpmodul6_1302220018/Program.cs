b64396576c096706bc0e090723c2d72707695287
AdrianIchiro <adrianfahren10@gmail.com>
2024-03-24 23:28:08 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302220018/Program.cs
+++ tpmodul6_1302220018/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(String judul)
+    {
+        this.id = new Random().Next(10000, 99999);
+        this.title = judul;
+        this.playCount = 0;
+    }
+
+    public void IncereasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine(this.id);
+        Console.WriteLine(this.title);
+        Console.WriteLine(this.playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Adrian");
+        video.IncereasePlayCount(1);
+        video.PrintVideoDetails();
+    }
+}
